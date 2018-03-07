ea0c1e84227155676a99825bd6a3a86806767081
ces9960 <ces9960@IGM2025-32>
2018-03-07 09:39:05 -0500

    Cullen

---
+++ Game1/Game1/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1
+{
+    abstract class Enemy : GameObject
+    {
+        public override void Die()
+        {
+
+        }
+    }
+
+}
---
+++ Game1/Game1/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1
+{
+    class Player : GameObject
+    {
+        public override void Move()
+        {
+
+        }
+        public override void Die()
+        {
+
+        }
+        public void Dash()
+        {
+
+        }
+        public void Jump()
+        {
+
+        }
+    }
+}
