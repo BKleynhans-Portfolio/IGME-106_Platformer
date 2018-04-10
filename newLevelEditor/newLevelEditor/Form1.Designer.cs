3272ccd586a1e15faf0a0ee5994e2fee22d9606c
Miranda Auriemma <msa4295@rit.edu>
2018-04-10 15:56:01 -0400

    starting ui

--- newLevelEditor/newLevelEditor/Form1.Designer.cs
+++ newLevelEditor/newLevelEditor/Form1.Designer.cs
@@ -28,31 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.Location = new System.Drawing.Point(36, 28);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(413, 329);
+            this.panel1.Size = new System.Drawing.Size(1040, 340);
             this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlLevel_Paint);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.openToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1133, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.newToolStripMenuItem.Text = "New";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(55, 398);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(497, 432);
+            this.ClientSize = new System.Drawing.Size(1133, 469);
+            this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.RadioButton radioButton1;
     }
 }
 
--- newLevelEditor/newLevelEditor/Form1.cs
+++ newLevelEditor/newLevelEditor/Form1.cs
@@ -12,9 +12,58 @@ namespace newLevelEditor
 {
     public partial class Form1 : Form
     {
+        int pixelSize;
+        Graphics graphics;
+        int pointX;
+        int pointY;
+        SolidBrush sb;
+       // Point point;
         public Form1()
         {
             InitializeComponent();
+
+            pixelSize = 20;
+
+            DrawGrid();
+
         }
+
+        private void pnlLevel_Paint(object sender, PaintEventArgs e)
+        {
+            graphics.FillRectangle(new SolidBrush(Color.Red), pointX, pointY, pixelSize, pixelSize);
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            pnlLevel_Paint(this, null);
+            DrawGrid();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DrawGrid();
+            //point = new Point(MousePosition.X, MousePosition.Y);
+        }
+
+        private void DrawGrid()
+        {
+            var relativePoint = panel1.PointToClient(Cursor.Position);
+            graphics = panel1.CreateGraphics();
+            pointX = relativePoint.X - (Math.Abs(relativePoint.X % pixelSize));
+            pointY = relativePoint.Y - (Math.Abs(relativePoint.Y % pixelSize));
+
+
+
+
+            for (int x = 0; x < pixelSize * (panel1.Height - 1 / pixelSize); x += pixelSize)
+            {
+                for (int y = 0; y < pixelSize * (panel1.Width - 1 / pixelSize); y += pixelSize)
+                {
+                    graphics.DrawRectangle(Pens.Purple, y, x, pixelSize, pixelSize);
+                }
+            }
+        }
+
+
     }
 }
